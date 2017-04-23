6b8c8f5ba52003845aa133fcee31bba6fb1fb2f5
shiimaxx <yoshima@shiimaxx.com>
2017-04-23 22:39:50 +0900

    WIP: Add ALDS1_2_D.cpp, ALDS1_3_A.cpp

---
+++ aizu-online-judge/ALDS1_2_D.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void insertionSort(int A, int n, int g) {
+
+}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    int A[n];
+
+    for ( int i = 0; i < n; i++ ) cin >> A[i];
+
+
+    return 0;
+}
---
+++ aizu-online-judge/ALDS1_3_A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 100;
+
+bool isEmpty(int top) {
+	if ( top == 0 ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool isFull(int top) {
+	if ( top == MAX ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void push(int S, int top) {
+	if ( isFull(top) ) {
+	} else {
+	}
+}
+
+
+int main() {
+	int a, b;
+	int top = 0;
+}
